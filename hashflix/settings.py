e57056c01cc05cdaef35d787919a1180a077ce89
RaimundoJorge1 <raimundojorge59@gmail.com>
2024-02-18 09:22:41 -0300

    Quinto

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -44,12 +44,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+ #   'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
