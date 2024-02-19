39e8cea362fd2d0b162c443595a415b540360c1d
RaimundoJorge1 <raimundojorge59@gmail.com>
2024-02-19 09:03:08 -0300

    Acertando o Set

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -30,7 +30,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://raiflix-production.up.railway.app/", "localhost","http://127.0.0.1/"]
+ALLOWED_HOSTS = ["raiflix-production.up.railway.app/", "localhost","127.0.0.1/"]
 
 
 # Application definition
