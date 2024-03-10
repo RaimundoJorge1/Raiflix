cf1e3ce2fa711061c60470bff425bc10b96a75a6
RaimundoJorge1 <raimundojorge59@gmail.com>
2024-03-10 07:22:12 -0300

    dia10.03.2024

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -31,8 +31,8 @@ else:
 DEBUG = True
 
 
-#ALLOWED_HOSTS = ["raiflix-production.up.railway.app", "localhost","127.0.0.1"]
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS = ["raiflix-production.up.railway.app", "localhost","127.0.0.1"]
+#ALLOWED_HOSTS =[]
 
 # Application definition
 
